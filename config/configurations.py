d2945ecfc7d4b8597d843942314ba6caaccef643
Farshad Koohifar <farshad.koohifar@gmail.com>
2019-02-13 22:31:02 -0500

    Just refactored main.py so that all parameters are in CONFIG.

---
+++ config/configurations.py
@@ -0,0 +1,48 @@
+# time horizon of a single rollout
+HORIZON = 200
+# number of rollouts per training iteration
+N_ROLLOUTS = 24
+# number of parallel workers
+N_CPUS = 6
+
+# grid_array
+INNER_LENGTH = 300
+LONG_LENGTH = 100
+SHORT_LENGTH = 300
+N_ROWS = 3
+N_COLUMNS = 3
+NUM_CARS_LEFT = 1
+NUM_CARS_RIGHT = 1
+NUM_CARS_TOP = 1
+NUM_CARS_BOT = 1
+
+# additional_env_params
+TARGET_VELOCITY = 50
+SWITCH_TIME = 3.0
+NUM_OBSERVED = 2
+DISCRETE = False
+TL_TYPE = 'controlled'
+
+# additional_net_params
+SPEED_LIMIT = 35
+HORIZONTAL_LANES = 1
+VERTICAL_LANES = 1
+
+# vehicles
+V_ENTER = 30
+MINGAP = 2.5
+
+# PPO
+ALG_RUN = 'PPO'
+GAMMA = 0.999
+HIDDEN_LAYERS = [32, 32]
+USE_GAE = True
+LAMBDA = 0.97
+KL_TARGET = 0.02
+NUM_SGD_ITER = 10
+CLIP_ACTIONS = False
+
+# Tune
+CHECKPOINT_FREQ = 20
+MAX_FAILURES = 999
+TRAINING_ITERATION = 200
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-"""Grid/green wave example."""
-
+"""Entry point to trainig RL for Ferocious Grid environment"""
+import os, sys, inspect
+cmd_folder = os.path.realpath(os.path.abspath(os.path.split(inspect.getfile( inspect.currentframe() ))[0]))
+if cmd_folder not in sys.path:
+    sys.path.insert(0, cmd_folder)
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"config")))
+if cmd_subfolder not in sys.path:
+    sys.path.insert(0, cmd_subfolder)
 import json
-
 import ray
 try:
     from ray.rllib.agents.agent import get_agent_class
@@ -9,57 +14,15 @@ except ImportError:
     from ray.rllib.agents.registry import get_agent_class
 from ray.tune import run_experiments
 from ray.tune.registry import register_env
-
 from flow.utils.registry import make_create_env
 from flow.utils.rllib import FlowParamsEncoder
 from flow.core.params import SumoParams, EnvParams, InitialConfig, NetParams, \
     InFlows, SumoCarFollowingParams
 from flow.core.params import VehicleParams
 from flow.controllers import SimCarFollowingController, GridRouter
+import config.configurations as CONFIG
 
-# time horizon of a single rollout
-HORIZON = 200
-# number of rollouts per training iteration
-N_ROLLOUTS = 24
-# number of parallel workers
-N_CPUS = 6
-
-
-def gen_edges(row_num, col_num):
-    edges = []
-    for i in range(col_num):
-        edges += ['left' + str(row_num) + '_' + str(i)]
-        edges += ['right' + '0' + '_' + str(i)]
-
-    # build the left and then the right edges
-    for i in range(row_num):
-        edges += ['bot' + str(i) + '_' + '0']
-        edges += ['top' + str(i) + '_' + str(col_num)]
-
-    return edges
-
-
-def get_flow_params(col_num, row_num, additional_net_params):
-    initial_config = InitialConfig(
-        spacing='custom', lanes_distribution=float('inf'), shuffle=True)
-
-    inflow = InFlows()
-    outer_edges = gen_edges(col_num, row_num)
-    for i in range(len(outer_edges)):
-        inflow.add(
-            veh_type='idm',
-            edge=outer_edges[i],
-            probability=0.25,
-            departLane='free',
-            departSpeed=20)
-
-    net_params = NetParams(
-        inflows=inflow,
-        no_internal_links=False,
-        additional_params=additional_net_params)
-
-    return initial_config, net_params
-
+TOTAL_CARS = (CONFIG.NUM_CARS_LEFT + CONFIG.NUM_CARS_RIGHT) * CONFIG.N_COLUMNS + (CONFIG.NUM_CARS_BOT + CONFIG.NUM_CARS_TOP) * CONFIG.N_ROWS
 
 def get_non_flow_params(enter_speed, additional_net_params):
     additional_init_params = {'enter_speed': enter_speed}
@@ -70,45 +33,31 @@ def get_non_flow_params(enter_speed, additional_net_params):
 
     return initial_config, net_params
 
-
-V_ENTER = 30
-INNER_LENGTH = 300
-LONG_LENGTH = 100
-SHORT_LENGTH = 300
-N_ROWS = 3
-N_COLUMNS = 3
-NUM_CARS_LEFT = 1
-NUM_CARS_RIGHT = 1
-NUM_CARS_TOP = 1
-NUM_CARS_BOT = 1
-tot_cars = (NUM_CARS_LEFT + NUM_CARS_RIGHT) * N_COLUMNS \
-           + (NUM_CARS_BOT + NUM_CARS_TOP) * N_ROWS
-
 grid_array = {
-    "short_length": SHORT_LENGTH,
-    "inner_length": INNER_LENGTH,
-    "long_length": LONG_LENGTH,
-    "row_num": N_ROWS,
-    "col_num": N_COLUMNS,
-    "cars_left": NUM_CARS_LEFT,
-    "cars_right": NUM_CARS_RIGHT,
-    "cars_top": NUM_CARS_TOP,
-    "cars_bot": NUM_CARS_BOT
+    "short_length": CONFIG.SHORT_LENGTH,
+    "inner_length": CONFIG.INNER_LENGTH,
+    "long_length": CONFIG.LONG_LENGTH,
+    "row_num": CONFIG.N_ROWS,
+    "col_num": CONFIG.N_COLUMNS,
+    "cars_left": CONFIG.NUM_CARS_LEFT,
+    "cars_right": CONFIG.NUM_CARS_RIGHT,
+    "cars_top": CONFIG.NUM_CARS_TOP,
+    "cars_bot": CONFIG.NUM_CARS_BOT
 }
 
 additional_env_params = {
-        'target_velocity': 50,
-        'switch_time': 3.0,
-        'num_observed': 2,
-        'discrete': False,
-        'tl_type': 'controlled'
+        'target_velocity': CONFIG.TARGET_VELOCITY,
+        'switch_time': CONFIG.SWITCH_TIME,
+        'num_observed': CONFIG.NUM_OBSERVED,
+        'discrete': CONFIG.DISCRETE,
+        'tl_type': CONFIG.TL_TYPE
     }
 
 additional_net_params = {
-    'speed_limit': 35,
+    'speed_limit': CONFIG.SPEED_LIMIT,
     'grid_array': grid_array,
-    'horizontal_lanes': 1,
-    'vertical_lanes': 1
+    'horizontal_lanes': CONFIG.HORIZONTAL_LANES,
+    'vertical_lanes': CONFIG.VERTICAL_LANES
 }
 
 vehicles = VehicleParams()
@@ -116,99 +65,73 @@ vehicles.add(
     veh_id='idm',
     acceleration_controller=(SimCarFollowingController, {}),
     car_following_params=SumoCarFollowingParams(
-        minGap=2.5,
-        max_speed=V_ENTER,
+        minGap=CONFIG.MINGAP,
+        max_speed=CONFIG.V_ENTER,
         speed_mode="all_checks",
     ),
     routing_controller=(GridRouter, {}),
-    num_vehicles=tot_cars)
+    num_vehicles=TOTAL_CARS)
 
-initial_config, net_params = \
-    get_non_flow_params(V_ENTER, additional_net_params)
+initial_config, net_params = get_non_flow_params(CONFIG.V_ENTER, additional_net_params)
 
 flow_params = dict(
-    # name of the experiment
-    exp_tag='green_wave',
-
-    # name of the flow environment the experiment is running on
+    exp_tag='ferocious',
     env_name='PO_TrafficLightGridEnv',
-
-    # name of the scenario class the experiment is running on
     scenario='SimpleGridScenario',
-
-    # simulator that is used by the experiment
     simulator='traci',
-
-    # sumo-related parameters (see flow.core.params.SumoParams)
     sim=SumoParams(
         sim_step=1,
         render=False,
     ),
-
-    # environment related parameters (see flow.core.params.EnvParams)
     env=EnvParams(
-        horizon=HORIZON,
+        horizon=CONFIG.HORIZON,
         additional_params=additional_env_params,
     ),
-
-    # network-related parameters (see flow.core.params.NetParams and the
-    # scenario's documentation or ADDITIONAL_NET_PARAMS component)
     net=net_params,
-
-    # vehicles to be placed in the network at the start of a rollout (see
-    # flow.core.vehicles.Vehicles)
     veh=vehicles,
-
-    # parameters specifying the positioning of vehicles upon initialization/
-    # reset (see flow.core.params.InitialConfig)
     initial=initial_config,
 )
 
-
 def setup_exps():
-
-    alg_run = 'PPO'
-
-    agent_cls = get_agent_class(alg_run)
+    agent_cls = get_agent_class(CONFIG.ALG_RUN)
     config = agent_cls._default_config.copy()
-    config['num_workers'] = N_CPUS
-    config['train_batch_size'] = HORIZON * N_ROLLOUTS
-    config['gamma'] = 0.999  # discount rate
-    config['model'].update({'fcnet_hiddens': [32, 32]})
-    config['use_gae'] = True
-    config['lambda'] = 0.97
-    config['kl_target'] = 0.02
-    config['num_sgd_iter'] = 10
-    config['clip_actions'] = False  # FIXME(ev) temporary ray bug
-    config['horizon'] = HORIZON
+    config['num_workers'] = CONFIG.N_CPUS
+    config['train_batch_size'] = CONFIG.HORIZON * CONFIG.N_ROLLOUTS
+    config['gamma'] = CONFIG.GAMMA  # discount rate
+    config['model'].update({'fcnet_hiddens': CONFIG.HIDDEN_LAYERS})
+    config['use_gae'] = CONFIG.USE_GAE
+    config['lambda'] = CONFIG.LAMBDA
+    config['kl_target'] = CONFIG.KL_TARGET
+    config['num_sgd_iter'] = CONFIG.NUM_SGD_ITER
+    config['clip_actions'] = CONFIG.CLIP_ACTIONS  # FIXME(ev) temporary ray bug
+    config['horizon'] = CONFIG.HORIZON
 
     # save the flow params for replay
     flow_json = json.dumps(
         flow_params, cls=FlowParamsEncoder, sort_keys=True, indent=4)
     config['env_config']['flow_params'] = flow_json
-    config['env_config']['run'] = alg_run
+    config['env_config']['run'] = CONFIG.ALG_RUN
 
     create_env, gym_name = make_create_env(params=flow_params, version=0)
 
     # Register as rllib env
     register_env(gym_name, create_env)
-    return alg_run, gym_name, config
-
+    return gym_name, config
 
 if __name__ == '__main__':
-    alg_run, gym_name, config = setup_exps()
-    ray.init(num_cpus=N_CPUS + 1, redirect_output=False)
+    gym_name, config = setup_exps()
+    ray.init(num_cpus=CONFIG.N_CPUS + 1, redirect_output=False)
     trials = run_experiments({
         flow_params['exp_tag']: {
-            'run': alg_run,
+            'run': CONFIG.ALG_RUN,
             'env': gym_name,
             'config': {
                 **config
             },
-            'checkpoint_freq': 20,
-            'max_failures': 999,
+            'checkpoint_freq': CONFIG.CHECKPOINT_FREQ,
+            'max_failures': CONFIG.MAX_FAILURES,
             'stop': {
-                'training_iteration': 200,
+                'training_iteration': CONFIG.TRAINING_ITERATION,
             },
         }
     })
